f0f36c8a7ca45c3006f679c405cc1fb5aa0bb836
sebuelias <sebuelias27@gmail.com>
2021-02-11 00:39:30 +0530

    duplicate letter, multiple argument check and null checks added

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,7 +1,9 @@
 function duplicateLetters(...args) {
+	if (args == null || args.length == 0) return false;
 	let maxCount = 0;
 	let count = 0;
-	for (x of args) {
+	for (let x of args) {
+		if (x == null || typeof x != "string") continue;
 		for (let i = 0; i < x.length - 1; i++) {
 			count = 1;
 			for (let j = i + 1; j < x.length; j++) {
