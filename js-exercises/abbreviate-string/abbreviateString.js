42ad4a918ba008227118f617d4ba6c786252f42e
sebuelias <sebuelias27@gmail.com>
2021-02-11 00:40:17 +0530

    abbreviate string, null check and string check added

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,9 +1,7 @@
-import { isString } from "util";
-
 function abbreviateString(str) {
+	if (str == null || typeof str != "string") return false;
 	let abbrev = str.trim().split(" ");
 	if (abbrev.length > 1) return abbrev[0] + " " + abbrev[1][0] + ".";
 	else return str;
 }
-
 export { abbreviateString };
