67e5a6e37ded4cb93fc42620ca88c7367ecb3ab7
sebuelias <sebuelias27@gmail.com>
2021-02-11 15:14:19 +0530

    Added throwing error on bad parameter, and es-lint issue fixes

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,7 +1,12 @@
 function abbreviateString(str) {
-	if (str == null || typeof str != "string") return false;
-	let abbrev = str.trim().split(" ");
-	if (abbrev.length > 1) return abbrev[0] + " " + abbrev[1][0] + ".";
-	else return str;
+  if (str == null || typeof str !== 'string') {
+    throw new Error('Invalid Parameters');
+  }
+  const abbrev = str.trim().split(' ');
+  if (abbrev.length > 1) {
+    /* First word and first letter of last word capitalised */
+    return `${abbrev[0]} ${abbrev[abbrev.length - 1][0].toUpperCase()}.`;
+  }
+  return str;
 }
 export { abbreviateString };
